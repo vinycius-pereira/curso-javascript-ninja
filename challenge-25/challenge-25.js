74a488596067a16b51ac4aecd5955fca143f8cb9
Vinícius Pereira <vinicius.pereira@waybee.com.br>
2021-07-21 14:25:26 -0300

    challenge-25 : finalizado revisão e pequeno projeto

--- challenge-25/challenge-25.js
+++ challenge-25/challenge-25.js
@@ -14,3 +14,48 @@ https://developer.mozilla.org/en-US/docs/Web/Events#Categories
 Tente aplicar na prática alguns dos eventos que estão ali e coloque nesse
 desafio os experimentos legais que você conseguir desenvolver :D
 */
+(function(win,doc){
+  'use-strict'
+
+  /*
+   button that turns on and off
+  - if on background should be white and title black
+  - if off background should be black and title white
+ */
+
+  function getElements(e) {
+    return doc.querySelector(e)
+  }
+
+  var buttonOn = getElements('[data-js="buttonOn"]');
+  var buttonOff = getElements('[data-js="buttonOff"]');
+  var title = getElements('[data-js="title"]')
+  var body = getElements('.body')
+
+  function initialize(){
+    initEvents()
+  }
+
+  function initEvents() {
+    buttonOn.addEventListener('click',turnOn,false)
+    buttonOff.addEventListener('click',turnOff,false)
+  }
+
+  function getElementColor(bodyColor,titleColor){
+    body.style.backgroundColor = bodyColor
+    title.style.color = titleColor
+  }
+
+  function turnOn() {
+    if(body.style.backgroundColor === 'white')
+      win.alert('It\'s already on');
+    getElementColor('white','black')
+  }
+  function turnOff() {
+    if(body.style.backgroundColor === 'black')
+      win.alert('It\'s already off');
+    getElementColor('black','white')
+  }
+
+  initialize()
+})(window,document);
